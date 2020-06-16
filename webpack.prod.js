f5ced230420c5991fca0054f9f4008b1f7dcd2b3
HARSH JAIN <jainharsh270@gmail.com>
2020-06-16 18:23:46 +0530

    minifying css file

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,12 +3,16 @@ const common = require("./webpack.common");
 const merge = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
   output: {
     filename: "[name].[contentHash].bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  optimization: {
+    minimizer: [new OptimizeCssAssetsPlugin()],
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" }),
